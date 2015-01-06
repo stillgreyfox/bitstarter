d67ea6443d79128cd53623a993f4d83da4410e54
stillgreyfox <me85638251@gmail.com>
2015-01-06 15:50:02 -0500

    changed app to load content from index.html file

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var express = require('express')
 var app = express();
+var fs = require('fs')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+/*
 app.get('/', function(request, response) {
   response.send('Hello World 2!')
 })
+*/
+
+// read the content from an external file instead of being hardcoded
+app.get('/', function(request, response) {
+  response.send(fs.readFileSync('index.html').toString())
+})
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
